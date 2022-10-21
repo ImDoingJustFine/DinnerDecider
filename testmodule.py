874976009e3f81ab5a941529ad17aa71f9a5d69e
Xander Colby <xandercolby@outlook.com>
2022-10-21 12:43:11 -0500

    shitweird-withcomments_10212022_1243

--- testmodule.py
+++ testmodule.py
@@ -14,14 +14,15 @@ class TestCases(unittest.TestCase):
     data = Meal_Data() # ---------------------------------------------> Instance of the data layer
     emptylist = [] # -------------------------------------------------------> Empty List for Clean Slate Test
 
-
+    # Works as intended
     def clean_slate(self):
         """Erases all data in foodinfo.json for testing"""
         
         with open(self.filename, 'w') as f:
             json.dump([], f)
             
-            
+    
+    # Works as intended        
     def test_clean_slate(self):
         """There should be no items in the list"""
 
@@ -43,7 +44,8 @@ class TestCases(unittest.TestCase):
         self.clean_slate()
         self.assertEqual(self.data.meal_get(), self.emptylist)
         
-        
+    
+    # Works as intended        
     def test_single_add(self):
         """Ability to write multiple  a single instance of the Meal class to foodinfo.json"""
         
@@ -52,40 +54,35 @@ class TestCases(unittest.TestCase):
         self.clean_slate()
         self.assertEqual(self.data.meal_get(), self.emptylist)
         self.data.meal_add(meal)
-        mealget = self.data.meal_get()
-        # testok = input(" Does foodinfo.json have one meals? ")
-        # if testok.title == "Yes":
-        #     pass
-        # else:
-        #     return
-        self.assertEqual(mealget[0].name, meal.name)
+        meal_gets = self.data.meal_get()
+        self.assertEqual(meal_gets[0].name, meal.name)
         self.clean_slate()
         self.assertEqual(self.data.meal_get(), self.emptylist)
     
-    
+    # Does not work as intended
+    # -- With all following test functions and lines 81 and 82 commented out, there should be instances of the Meal class stored in a list inside foodinfo.json
+    # -- Instead, we are left with an empty list though the code supposedly passes all tests
     def test_multiple_add(self):
         """Ability to write multiple instances of the Meal class to foodinfo.json"""
 
         meal0 = Meal("fried chicken sandwhich", "chicken", 2, 3) # --------------> Meal Object for Multiple Add Test (Goal: Add)
         meal1 = Meal("steak", "beef", 5, 1) # -----------------------------------> Meal Object for Multiple Add Test (Goal: Add)
         
-        self.clean_slate()
-        self.assertEqual(self.data.meal_get(), self.emptylist)
-        self.data.meal_add(meal0)
-        mealget0 = self.data.meal_get()
-        self.data.meal_add(meal1)
-        mealget1 = self.data.meal_get()
-        # testok = input(" Does foodinfo.json have two meals? ")
-        # if testok.title == "Yes":
-        #     pass
-        # else:
-        #     return
-        self.assertEqual(mealget0[0].name, meal0.name)
-        self.assertEqual(mealget1[1].name, meal1.name)
-        self.clean_slate()
-        self.assertEqual(self.data.meal_get(), self.emptylist)
+        self.clean_slate() # -- >> Overwrites whatever's in foodinfo.json to an empty list
+        self.assertEqual(self.data.meal_get(), self.emptylist) # -- >> Makes sure self.clean_slate() did its job
+        self.data.meal_add(meal0) # ------------ \
+        meal_gets = self.data.meal_get() # ----- >> Adds meal0 (fried chicken sandwich) to foodinfo.json and checks that there is only one item in the list
+        self.assertEqual(len(meal_gets), 1) # -- /
+        self.data.meal_add(meal1) # ------------ \
+        meal_gets = self.data.meal_get() # ----- >> Adds meal1 (steak) to foodinfo.json and checks that there are two items in the list
+        self.assertEqual(len(meal_gets), 2) # -- /
+        self.assertEqual(meal_gets[0].name, meal0.name) # -- >> Checks that the first meal from foodinfo.json is meal0 (fried chicken sandwhich)
+        self.assertEqual(meal_gets[1].name, meal1.name) # -- >> Checks that the second meal from foodinfo.json is meal1 (steak)
+        self.clean_slate() # -- >> Overwrites whatever's in foodinfo.json to an empty list
+        self.assertEqual(self.data.meal_get(), self.emptylist) # -- >> Makes sure self.clean_slate() did its job
      
-        
+    
+    # Works as intended
     def test_float(self):
         """Floats are rounded and converted into integers"""
         
@@ -99,6 +96,7 @@ class TestCases(unittest.TestCase):
         self.assertEqual(self.data.meal_get(), self.emptylist)
     
     
+    # Works as inteded
     def test_def_limit(self):
         """Cost and Difficulty values are between 1 and 5"""
         
@@ -114,7 +112,7 @@ class TestCases(unittest.TestCase):
         self.clean_slate()
         self.assertEqual(self.data.meal_get(), self.emptylist)
        
-        
+    # Works as intended    
     def test_single_delete(self):
         """Taco meal should be removed from the list"""
         
@@ -135,10 +133,15 @@ class TestCases(unittest.TestCase):
         self.data.meal_del(meal1.name)
         meal_gets = self.data.meal_get()
         self.assertEqual(meal_gets[0].name, meal2.name)
-        self.assertEqual(meal_gets[1].name, meal3.name)
-        self.clean_slate()
-        self.assertEqual(self.data.meal_get(), self.emptylist)
-        
+        self.assertEqual(meal_gets[1].name, meal3.name)   
+        # self.clean_slate() # -- >> Overwrites whatever's in foodinfo.json to an empty list
+        # self.assertEqual(self.data.meal_get(), self.emptylist) # -- >> Makes sure self.clean_slate() did its job
+    
+    
+    # Does not work as intended
+    # -- With lines 137, 138, 172, and 173 commented out, there should be one instance of the Meal class in a list inside foodinfo.json
+    # -- Instead foodinfo.json contains a list with two instances of the meal class in a list inside foodinfo.json
+    # -- Somehow, the code supposedly passes all tests
     def test_multiple_delete(self):
         """Taco and Fried Chicken Sandwhich should be removed from the list"""
         
@@ -147,7 +150,7 @@ class TestCases(unittest.TestCase):
         meal3 = Meal("steak", "beef", 5, 1) # ------------------------------> Meal Object for Single Delete Test (Goal: Add)
         
         self.clean_slate() # -- >> Overwrites whatever's in foodinfo.json to an empty list
-        self.assertEqual(self.data.meal_get(), self.emptylist) # -- >> makes sure self.clean_slate() did its job
+        self.assertEqual(self.data.meal_get(), self.emptylist) # -- >> Makes sure self.clean_slate() did its job
         self.data.meal_add(meal1) # ------------ \
         meal_gets = self.data.meal_get() # ----- >> Adds meal1 (taco) to foodinfo.json and checks that there is only one item in the list
         self.assertEqual(len(meal_gets), 1) # -- /
@@ -166,8 +169,8 @@ class TestCases(unittest.TestCase):
         meal_gets = self.data.meal_get() # ----- \
         self.assertEqual(len(meal_gets), 1) # -- >> Ensures there is only one remaining item in the list
         self.assertEqual(meal_gets[0].name, meal3.name) # -- >> Ensures the remaining item is meal3 (steak)
-        # self.clean_slate() # -- >> Overwrites whatever's in foodinfo.json to an empty list
-        # self.assertEqual(self.data.meal_get(), self.emptylist) # -- >> makes sure self.clean_slate() did its job
+    #     # self.clean_slate() # -- >> Overwrites whatever's in foodinfo.json to an empty list
+    #     # self.assertEqual(self.data.meal_get(), self.emptylist) # -- >> Makes sure self.clean_slate() did its job
   
 if __name__ == '__main__':
     unittest.main()
